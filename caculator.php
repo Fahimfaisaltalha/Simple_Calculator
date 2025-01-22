aab2d7be665926e2a74a98cdc2d4b286542564e8
Fahimfaisaltalha <faisal15-4721@diu.edu.bd>
2025-01-23 02:45:06 +0600

    Summation Part

--- caculator.php
+++ caculator.php
@@ -1,6 +1,16 @@
 <?php
 
-
+echo "Enter the Value You want to Calculate\n";
+$firstValue=readline("Enter the First Value = ");
+$secondValue=readline("Enter the Second Value = ");
+echo "Enter the arithmetic operators symbol you want for you calculation\n";
+echo "Example : +  - * / \n";
+echo "\n";
+$symbol=readline("Enter the Symbol  : ");
+if($symbol == "+"){
+    $sum=$firstValue+$secondValue;
+    echo "The Summation of two number is : $sum";
+}
 
 
 ?>
